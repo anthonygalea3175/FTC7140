b7c6191801548cc210288d61144a279e5bcf45cf
anthonygalea3175 <galeaac@rose-hulman.edu>
2016-10-30 17:25:48 -0400

    Add files via upload

---
+++ teamcode/Code7140.java
@@ -0,0 +1,141 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.teamcode.Hardware7140;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the Hardware7140 class.
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a POV Game style Teleop for a PushBot
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="TeleOp V1", group="7140")
+public class Code7140 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    Hardware7140 robot           = new Hardware7140();   // Use 7140's hardware
+                                                               // could also use HardwarePushbotMatrix class
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double max;
+        double intake;
+        double shooter;
+        double liftPower1;
+        double liftPower2;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            left  = -gamepad1.left_stick_y + gamepad1.right_stick_x;
+            right = -gamepad1.left_stick_y - gamepad1.right_stick_x;
+            intake = gamepad2.right_stick_y - gamepad2.right_stick_x;
+            shooter = gamepad2.left_stick_y + gamepad2.right_stick_x;
+            liftPower1 = gamepad2.left_trigger;
+            liftPower2 = gamepad2.right_trigger;
+
+            // Normalize the values so neither exceed +/- 1.0
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }
+
+            robot.leftMotor1.setPower(left);
+            robot.leftMotor2.setPower(left);
+            robot.rightMotor1.setPower(right);
+            robot.rightMotor2.setPower(right);
+            robot.shooter1.setPower(shooter);
+            robot.shooter2.setPower(shooter);
+
+            //surgical tubing lift
+            if(liftPower1 < 0.1) {
+                if(liftPower2 > 0.1) {
+                    robot.lift.setPower(liftPower1);
+                }
+                else {
+                    robot.lift.setPower(0);
+                }
+            }
+            if (liftPower2 < 0.1){
+                if(liftPower1 > 0.1) {
+                    robot.lift.setPower(liftPower2);
+                }
+                else {
+                    robot.lift.setPower(0);
+                }
+            }
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("left",  "%.2f", left);
+            telemetry.addData("right", "%.2f", right);
+            telemetry.addData("shooter","&.2f", shooter);
+            telemetry.addData("intake","&.2f", intake);
+            telemetry.update();
+
+            // Pause for metronome tick.  40 mS each cycle = update 25 times a second.
+            robot.waitForTick(40);
+        }
+    }
+}
